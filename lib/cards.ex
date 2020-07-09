453ceacb12e25b5f2daa09af85f0b6b4a6b43dc1
zenobio <zenobio2190@gmail.com>
2020-07-09 17:13:18 -0300

    Added doctest and some test changes

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,11 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Methods to create and handling a deck of cards.
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades","Clubs", "Hearts", "Diamonds"]
@@ -13,33 +16,71 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffle a given deck.
+  """
   def shuffle(deck), do: Enum.shuffle(deck)
 
 # There's nothing special with ?, it's just a way to point to others
 # that this function will return a true/false value.
+  @doc """
+    Check if a given deck contains a given card
+
+  ## Example
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card), do: Enum.member?(deck, card)
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    the `hand_size` argument indicates how many cards a hand
+    should have.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 2)
+      iex> hand
+      ["Ace of Spades", "Two of Spades"]
+  """
   def deal(deck, hand_size) do
-    deck
-    |> Enum.split(hand_size)
-    |> elem(0)
+    Enum.split(deck, hand_size)
   end
 
-  # def save(deck, file_name) do
-  #   binary = :erlang.term_to_binary(deck)
-  #   File.write(filename, binary)
-  # end
+  @doc """
+    Saves a given deck at a local file
+  """
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(file_name, binary)
+  end
 
-  # def load(file_name) do
-  #   case File.read(fine_name) do
-  #     {:ok, binary} -> :erlang.binary_to_term(binary)
-  #     {:error, _reason} -> "File not found"
-  #   end
-  # end
+  @doc """
+    Loads a deck from a given local file.
+  """
+  def load(file_name) do
+    case File.read(file_name) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File not found"
+    end
+  end
 
+  @doc """
+    Create and returns a hand of a given size.
+
+  ## Example
+
+      hand = Cards.create_hand(2)
+      hand
+      ["Five of Spades", "Five of Hearts"]
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
     |> Cards.deal(hand_size)
+    |> elem(0)
   end
 end
